5c62f009fd9759d008842587b800aef5214f34ca
Abel Trotter and Brandon Betts <abel@>
2018-05-29 10:44:38 -0700

    add initial html, css, js, and readme files, create initial gallons/liters converter

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function gallonsToLiters() {
+  var gallons = prompt("Enter number of gallons:");
+  var liters = gallons * 3.785411784;
+  alert(gallons + " gallons is equal to " + liters + " liters");
+}
+gallonsToLiters();
